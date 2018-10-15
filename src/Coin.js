5863b0e95e20b44f0759e7c5e3110e4de9b5c0c7
Benjamin JEHL <benjaminjehl@Benjamins-MBP.localdomain>
2018-10-15 16:31:29 -0400

    changed interval time

--- src/Coin.js
+++ src/Coin.js
@@ -1,59 +1,57 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class Coin extends Component {
+  constructor(props) {
+    super(props)
 
-    constructor(props) {
-        super(props)
+    this.state = {
+      priceIncreasing: false,
+      priceDecreasing: false
+    }
+  }
 
-        this.state = {
-            priceIncreasing: false,
-            priceDecreasing: false
+  componentWillReceiveProps = newProps => {
+    if (newProps.price > this.props.price) {
+      this.setState((state, props) => {
+        return {
+          priceIncreasing: true
         }
+      })
+    }
+    if (newProps.price < this.props.price) {
+      this.setState((state, props) => {
+        return {
+          priceDecreasing: true
+        }
+      })
+    }
+  }
+
+  render() {
+    let priceClass = 'normal'
+
+    if (this.state.priceIncreasing) {
+      priceClass = 'green'
     }
 
-    componentWillReceiveProps = newProps => {
-        if (newProps.price > this.props.price) {
-            this.setState((state, props) => {
-                return {
-                    priceIncreasing: true
-                }
-            })
-        }
-        if (newProps.price < this.props.price) {
-            this.setState((state, props) => {
-                return {
-                    priceDecreasing: true
-                }
-            })
-        }
+    if (this.state.priceDecreasing) {
+      priceClass = 'red'
     }
 
-    render() {
-        let priceClass = "normal"
-
-        if (this.state.priceIncreasing) {
-            priceClass = "green"
-        }
-
-        if (this.state.priceDecreasing) {
-            priceClass = "red"
-        }
-
-        return (
-            <tr className={priceClass}>
-                <td>{this.props.name}</td>
-                <td>{this.props.symbol}</td>
-                <td>{this.props.circulating}</td>
-                <td>${this.props.price}</td>
-                <td>{this.props.volume}</td>
-                <td>${this.props.marketCap}</td>
-                <td>{this.props.lastHour}%</td>
-                <td>{this.props.lastTwentyFour}%</td>
-                <td>{this.props.lastSeven}%</td>
-            </tr>
-        )
-    }
+    return (
+      <tr className={priceClass}>
+        <td>{this.props.name}</td>
+        <td>{this.props.symbol}</td>
+        <td>{this.props.circulating}</td>
+        <td>${this.props.price}</td>
+        <td>{this.props.volume}</td>
+        <td>${this.props.marketCap}</td>
+        <td>{this.props.lastHour}%</td>
+        <td>{this.props.lastTwentyFour}%</td>
+        <td>{this.props.lastSeven}%</td>
+      </tr>
+    )
+  }
 }
 
-export default Coin;
-
+export default Coin
--- src/MainTable.js
+++ src/MainTable.js
@@ -1,76 +1,68 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import Coin from './Coin.js'
 // import coinData from './coinData.json'
 
-
 class MainTable extends Component {
+  constructor(props) {
+    super(props)
 
-    constructor(props) {
-        super(props)
+    this.state = { coins: [] }
+  }
 
-        this.state = { coins: [] }
-    }
-
-    componentDidMount() {
-        setInterval(() => {
-            fetch('https://api.coinmarketcap.com/v2/ticker/?limit=100')
-                .then(response => {
-                    return response.json()
-                })
-                .then(myJson => {
-                    this.setState({ coins: myJson.data })
-                })
-        }, 1000)
-    }
-
-    render() {
-        let coins = Object.keys(this.state.coins).map(id => {
-            let coin = this.state.coins[id]
-            return <Coin
-                key={coin.id}
-                name={coin.name}
-                symbol={coin.symbol}
-                circulating={coin.circulating_supply}
-                price={coin.quotes.USD.price}
-                volume={coin.quotes.USD.volume_24h}
-                marketCap={coin.quotes.USD.market_cap}
-                lastHour={coin.quotes.USD.percent_change_1h}
-                lastTwentyFour={coin.quotes.USD.percent_change_24h}
-                lastSeven={coin.quotes.USD.percent_change_7d}
-            />
+  componentDidMount() {
+    setInterval(() => {
+      fetch('https://api.coinmarketcap.com/v2/ticker/?limit=100')
+        .then(response => {
+          return response.json()
         })
+        .then(myJson => {
+          this.setState({ coins: myJson.data })
+        })
+    }, 10000)
+  }
 
-        return (
-            <table>
+  render() {
+    let coins = Object.keys(this.state.coins).map(id => {
+      let coin = this.state.coins[id]
+      return (
+        <Coin
+          key={coin.id}
+          name={coin.name}
+          symbol={coin.symbol}
+          circulating={coin.circulating_supply}
+          price={coin.quotes.USD.price}
+          volume={coin.quotes.USD.volume_24h}
+          marketCap={coin.quotes.USD.market_cap}
+          lastHour={coin.quotes.USD.percent_change_1h}
+          lastTwentyFour={coin.quotes.USD.percent_change_24h}
+          lastSeven={coin.quotes.USD.percent_change_7d}
+        />
+      )
+    })
 
-                <thead>
-                    <tr>
-                        <th colSpan="9">CryptoCurrency Data</th>
-                    </tr>
-                    <tr className="general-data">
-                        <td>Name</td>
-                        <td>Symbol</td>
-                        <td>Circulating Supply</td>
-                        <td>Price</td>
-                        <td>Volume last 24 hours</td>
-                        <td>Market Cap</td>
-                        <td>% change last hour</td>
-                        <td>% change last 24 hours</td>
-                        <td>% change last 7 days</td>
-                    </tr>
-                </thead>
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th colSpan="9">CryptoCurrency Data</th>
+          </tr>
+          <tr className="general-data">
+            <td>Name</td>
+            <td>Symbol</td>
+            <td>Circulating Supply</td>
+            <td>Price</td>
+            <td>Volume last 24 hours</td>
+            <td>Market Cap</td>
+            <td>% change last hour</td>
+            <td>% change last 24 hours</td>
+            <td>% change last 7 days</td>
+          </tr>
+        </thead>
 
-                <tbody>
-
-                    {coins}
-
-                </tbody>
-
-            </table >
-        )
-    }
+        <tbody>{coins}</tbody>
+      </table>
+    )
+  }
 }
 
-export default MainTable;
-
-
+export default MainTable
